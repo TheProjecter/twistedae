13c7c8d9fb718d0d2e2183273b6b95c729501783
tobias.rodaebel <tobias.rodaebel@c8515886-77aa-11de-8a01-315fa775be5f>
2009-08-09 21:09:42 +0000

    Deleted unused variable.

--- src/twistedae/tests/test_memcache.py
+++ src/twistedae/tests/test_memcache.py
@@ -64,7 +64,6 @@ class MemcacheTestCase(twisted.trial.unittest.TestCase):
 
         first = "Little pig, little pig, let me come in!"
         second = "Not by the hair on my chinny-chin-chin!"
-        third = "Then I'll huff, and I'll puff, and I'll blow your house in!"
         google.appengine.api.memcache.add('first', first)
         assert google.appengine.api.memcache.get('first') == first
         google.appengine.api.memcache.replace('first', second)
