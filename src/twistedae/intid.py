4e70e1c0af07992081c2f40eefb9eebcd8c2f89e
tobias.rodaebel <tobias.rodaebel@c8515886-77aa-11de-8a01-315fa775be5f>
2009-09-12 23:01:15 +0000

    Refactored.

--- src/twistedae/intid.py
+++ src/twistedae/intid.py
@@ -1,17 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Tobias Rodäbel
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Simple intid server implementation."""
+
+import logging
 import pymongo.connection
+import signal
 import socket
 import threading
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("", 9009))
-server_socket.listen(5)
+logging.basicConfig(
+    format='%(levelname)-8s %(asctime)s %(filename)s:%(lineno)s] %(message)s',
+    level=logging.INFO)
 
 lock = threading.Lock()
-
 db = pymongo.connection.Connection()['intid']
 
+
 def increment(delta=1):
-    """ Returns incremented intid."""
+    """Returns incremented intid."""
+
     intid = db.intid
     result = intid.find_one()
     if result is None:
@@ -21,9 +41,12 @@ def increment(delta=1):
     intid.update(result, {"$set": {u'i': value}})
     return value
 
-class Client(threading.Thread):
+
+class Worker(threading.Thread):
+    """The worker thread."""
+
     def __init__(self, sock):
-        super(Client, self).__init__()
+        super(Worker, self).__init__()
         self.socket = sock
 
     def run(self):
@@ -38,8 +61,24 @@ class Client(threading.Thread):
                 self.socket.close()
                 break
 
+
 def main():
-    while 1:
-        socketfd, address = server_socket.accept()
-        client = Client(socketfd)
-        client.run()
+    """The main function."""
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 9009))
+    server_socket.listen(10)
+
+    logging.info("server starting")
+
+    client_num = 0
+
+    try:
+        while 1:
+            socketfd, address = server_socket.accept()
+            client_num += 1
+            logging.info("client %i %s connected" % (client_num, address))
+            t = Worker(socketfd)
+            t.run()
+    finally:
+        server_socket.close() 
